8fc981547cf31f3666e46323d9280e9169710677
Nicholas Solomon <nicholassolomon99@outlook.com>
2024-10-23 11:33:47 -0700

    Enhanced Input

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,5 @@
+// Copyright (c) 2024 Nicholas Solomon All Rights Reserved
+
+
+#include "Player/AuraPlayerController.h"
+
---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2024 Nicholas Solomon All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
