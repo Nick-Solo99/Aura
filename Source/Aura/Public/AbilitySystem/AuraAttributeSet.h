8b81276205c4946755c1c3e6bdfeab9ed5396150
Nicholas Solomon <nicholassolomon99@outlook.com>
2024-10-23 16:47:06 -0700

    Ability System Component and Attribute Set

---
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2024 Nicholas Solomon All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AuraAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
