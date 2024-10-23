8084ef716db923027a50e730ff18b385f06f0be7
Nicholas Solomon <nicholassolomon99@outlook.com>
2024-10-23 13:44:33 -0700

    Game Mode

---
+++ Source/Aura/Public/Game/AuraGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2024 Nicholas Solomon All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
